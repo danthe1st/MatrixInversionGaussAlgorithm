184a96717999cc70ab10dbd41b5139ef55e17925
FabianSpiessberger <43297745+FabianSpiessberger@users.noreply.github.com>
2023-01-03 12:14:27 +0100

    Create ReducedRowEchelon.py

---
+++ ReducedRowEchelon.py
@@ -0,0 +1,71 @@
+from Matrix import SimpleMatrix
+
+
+def to_reduced_row_echelon_form(matrix: SimpleMatrix):
+    # check if there even is a matrix
+    if matrix is None:
+        return
+
+    column_index = 0
+    row_count = matrix.row_count()
+    column_count = matrix.column_count()
+
+    # iterate row by row
+    for row_index in range(row_count):
+
+        # check if index does not exceed the number of columns of the matrix
+        if column_index >= column_count:
+            return  # return if the index exceeds the number of columns
+
+        # start at current row
+        row_iterator = row_index
+
+        # check for zero-entries
+        while matrix.get_element(row_iterator, column_index) == 0:
+
+            row_iterator += 1
+            if row_iterator == row_count:
+                row_iterator = row_index
+                column_index += 1
+                if column_count == column_index:
+                    return
+
+        # row iterator might be equals row index -> no swap required
+        if row_iterator != row_index:
+            matrix.swap_rows(row_iterator, row_index)
+
+        # get element at current position
+        elem = matrix.get_element(row_index, column_index)
+
+        # divide each element of the current row by the elem
+        for rc in range(column_count):
+            matrix.set_element(row_index, rc, matrix.get_element(row_index, rc) / float(elem))
+
+        # do this for all rows but the current one
+        for i_row in range(row_count):
+            if i_row != row_index:
+
+                # get value
+                elem = matrix.get_element(i_row, column_index)
+
+                # subtract one column from another - the current one is skipped and used for the subtraction
+                for ir in range(column_count):
+                    value = matrix.get_element(i_row, ir) - elem * matrix.get_element(row_index, ir)
+                    matrix.set_element(i_row, ir, value)
+
+        column_index += 1  # go to next column
+
+
+if __name__ == '__main__':
+
+    # values = [[1, 2, -1, -4], [2, 3, -1, -11], [-2, 0, -3, 22]]
+    values = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    mx = SimpleMatrix(len(values), len(values.__getitem__(0)))
+
+    mx.fill(values)
+
+    to_reduced_row_echelon_form(mx)
+
+    for i in range(mx.row_count()):
+        print([int(value) for value in mx.data.__getitem__(i)])
